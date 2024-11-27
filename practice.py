e96eae370e93a5af41c9d217d6f836e7a434a18c
youl11 <youlkim20@gmail.com>
2024-11-28 02:15:54 +0900

    11/27

--- practice.py
+++ practice.py
@@ -1,26 +1,22 @@
 # 👇🏻 YOUR CODE 👇🏻:
 def get_yearly_revenue(monthly_revenue):
-    return monthly_revenue*12
+    return monthly_revenue * 12
+
 def get_yearly_expenses(monthly_expenses):
-    return monthly_expenses*12
+    return monthly_expenses * 12
 
-yearly_revenue= get_yearly_revenue
-yearly_expenses=get_yearly_expenses
+def get_tax_amount(profit):
+    if profit > 100000:
+        return profit * 0.25
+    else:
+        return profit * 0.15
 
-def profit(yearly_revenue, yearly_expenses):
-    return yearly_revenue-yearly_expenses
-
-def tax_amout(profit):
-    return profit*0.1
+def apply_tax_credits(tax_amount, tax_credits):
+    return tax_amount * tax_credits
 
 def final_tax_amount(tax_amount, tax_credits):
-    return tax_amount - tax_credits
-
-print(f"Your tax bill is : ${final_tax_amount}")
-monthly_revenue = 5500000
-monthly_expenses = 2700000
-tax_credits = 0.01
-
+    return tax_amount-tax_credits
+print(f"Your tax bill is: ${final_tax_amount}")
 # /YOUR CODE
 
 # BLUEPRINT | DONT EDIT
